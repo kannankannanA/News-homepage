367332d4ab3156706beb10765a158081e12e0ba9
ARTIST <149696101+kannankannanA@users.noreply.github.com>
2024-03-13 16:13:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//kannan follow https://github.com/kannankannanA
+
+let button=document.querySelector(".arrow"),
+    toggle=document.querySelector(".nav");
+    
+
+     button.addEventListener("click", () => {
+    toggle.classList.toggle("display");
+    button.children[0].classList.toggle("jk_display1");
+    button.children[1].classList.toggle("jk_display2");
+    button.children[2].classList.toggle("jk_display3");
+    
+    
+})
+//kannan follow https://github.com/kannankannanA
+
+
